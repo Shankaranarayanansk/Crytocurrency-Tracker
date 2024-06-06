50512645d2038fbd2c38a49de53f4aa24c08b5e6
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-06-06 21:51:25 +0530

    home done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { useContext } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import arrow from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
 const Navbar = () => {
+  const {setcurrency} = useContext(CoinContext)
+  const handleCurrency = (e) => {
+    switch (e.target.value) {
+      case "usd":
+        {
+        setcurrency({name: "usd", symbol: "$"})
+        break
+        }
+      case "inr":
+        {
+        setcurrency({name: "inr", symbol: "₹"})
+        break
+        }
+        
+      case "eur":
+        {
+        setcurrency({name: "eur", symbol: "€"})
+        break
+        }
+        default:
+          {
+            setcurrency({name: "usd", symbol: "$"})
+          }
+    }
+  }
   return (
     <div className='navbar'>
       <img src={logo}  className='logo' alt="" />
@@ -13,7 +40,7 @@ const Navbar = () => {
             <li>Blog</li>
         </ul>
         <div className='nav-right'>
-            <select >
+            <select onChange={handleCurrency}>
                 <option value="usd">USD</option>
                 <option value="inr">INR</option>
                 <option value="eur">EUR</option>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
+import { useContext } from 'react'
+import { CoinContext } from '../../context/CoinContext'
 
 const Home = () => {
-  return (
-    <div>Home</div>
+    const {allCoin,currency} = useContext(CoinContext);
+    const [displayCoin,setDisplayCoin] = useState([])
+    useEffect(() => {
+        setDisplayCoin(allCoin)
+        
+    }),[allCoin]
+    return (
+    <div className='home'>
+        <div className='hero'>
+            <h1>Largest <br />Crypto Tacker Site</h1>
+            <p>Welcome to the world of cryptocurrency.Lets sign up to get started</p>
+            <form>
+                <input type="text" placeholder='Search Crypto'/>
+                <button type='submit'>Search</button>
+            </form>
+        </div>
+        <div className="crypto-table">
+            <div className="table-layout">
+                <p>#</p>
+                <p>Coin</p>
+                <p>Price</p>
+                <p  style={{textAlign: 'center'}}>24h Change</p>
+                <p className='market'>Market Cap</p>
+            </div>
+            {
+                displayCoin.slice
+            }
+        </div>
+    </div>
   )
 }
 
